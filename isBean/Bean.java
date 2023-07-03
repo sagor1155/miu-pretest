8402b88224e9167f450d452132222ab655fa76b8
sagor <sagor.eee.cuet@gmail.com>
2023-07-04 00:33:26 +0600

    solve Bean

--- isBean/Bean.java
+++ isBean/Bean.java
@@ -1,13 +1,27 @@
 public class Bean {
     public static void main(String[] args) {
-        System.out.println(isBean(new int[]{4,9,8}));
-        System.out.println(isBean(new int[]{2,2,5,11,23}));
-        System.out.println(isBean(new int[]{7,7,3,6}));
-        System.out.println(isBean(new int[]{0}));
-        System.out.println(isBean(new int[]{3,8,4}));
+        System.out.println(isBean(new int[]{4,9,8}));       // 1
+        System.out.println(isBean(new int[]{2,2,5,11,23})); // 1
+        System.out.println(isBean(new int[]{7,7,3,6}));     // 1
+        System.out.println(isBean(new int[]{0}));           // 1
+        System.out.println(isBean(new int[]{3,8,4}));       // 0
     }
 
     public static int isBean(int[] a) {
-        return 0;
+        if (a.length==0) return 0;
+
+        for (int i=0; i<a.length; i++) {
+            int elem = a[i];
+            boolean hasBean = false;
+            for (int k=0; k<a.length; k++) {
+                if (elem == a[k]*2 || elem == (a[k]*2+1) || elem == a[k]/2) {
+                    hasBean = true;
+                    break;
+                }
+            }
+            if (!hasBean) return 0;
+        }
+
+        return 1;
     }
 }
--- isBean/Readme.txt
+++ isBean/Readme.txt
@@ -6,6 +6,8 @@ for example {4,9,8} is a Bean array because for 4,8 is present, for 9, 4 is pres
 Other Bean arrays include {2,2,5,11,23}, {7,7,3,6} and {0}
 The array {3,8,4} is not a Bean array because of the value 3 which requires that the array contains
 either the value 6, 7 or 1 and none of these values are in the array.
+
 Write a function named isBean that returns 1 if it's array argument is a Bean array,
 otherwise it returns a 0.
+
 if you are programming in Java or C#, the function signature is int isBean(int[] a)
