13a4540f854e0963fa54e5ae28c105b4e9d19ac6
sagor <sagor.eee.cuet@gmail.com>
2023-07-05 03:10:27 +0600

    solve Olympic

--- isOlympic/Olympic.java
+++ isOlympic/Olympic.java
@@ -1,14 +1,28 @@
 public class Olympic {
     public static void main(String[] args) {
-        System.out.println(isOlympic(new int[]{3, 2, 1}));
-        System.out.println(isOlympic(new int[]{2, 2, 1, 1}));
-        System.out.println(isOlympic(new int[]{1, 1000, 100, 10000, 2}));
-        System.out.println(isOlympic(new int[]{1, 99, 99, 1000, 100, 10000, 2}));
-        System.out.println(isOlympic(new int[]{1, 2, 1, 3, 2}));
-        System.out.println(isOlympic(new int[]{1, 2, -1, 2, 2}));
+        System.out.println(isOlympic(new int[]{3, 2, 1}));  // 1
+        System.out.println(isOlympic(new int[]{2, 2, 1, 1}));   // 1
+        System.out.println(isOlympic(new int[]{1, 1000, 100, 10000, 2}));   // 1
+        System.out.println(isOlympic(new int[]{1, 99, 99, 1000, 100, 10000, 2}));   // 0
+        System.out.println(isOlympic(new int[]{1, 2, 1, 3, 2}));    // 0
+        System.out.println(isOlympic(new int[]{1, 2, -1, 2, 2}));   // 0
     }
 
     public static int isOlympic(int[] nums) {
-        return 0;
+        if (nums.length==0) return 0;
+        if (nums.length==1) return 1;
+
+        for (int i=0; i<nums.length; i++) {
+            int val = nums[i];
+            int sum = 0;
+            for (int k=0; k<nums.length; k++) {
+                if (i!=k && nums[k]<val) {
+                    sum += nums[k];
+                }
+            }
+            if (val < sum) return 0;
+        }
+
+        return 1;
     }
 }
