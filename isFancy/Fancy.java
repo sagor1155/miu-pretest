d2bcb6c81e09596aa0685ebf597222f89926ffe1
sagor <sagor.eee.cuet@gmail.com>
2023-07-05 02:29:22 +0600

    solve Fancy

--- isFancy/Fancy.java
+++ isFancy/Fancy.java
@@ -8,6 +8,16 @@ public class Fancy {
     }
 
     public static int isFancy(int n) {
+        if (n <= 0) return 0;
+        int first = 1;
+        int second = 1;
+
+        while (second < n) {
+            int temp = second;
+            second = second*3 + first*2;
+            first = temp;
+        }
+        if (second == n) return 1;
         return 0;
     }
 }
