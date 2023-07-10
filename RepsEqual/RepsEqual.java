86b981c70d073e37424ba5ed299a90132d428d86
sagor <sagor.eee.cuet@gmail.com>
2023-07-11 02:25:16 +0600

    solve RepsEqual

--- RepsEqual/RepsEqual.java
+++ RepsEqual/RepsEqual.java
@@ -7,9 +7,16 @@ public class RepsEqual {
         System.out.println(repsEqual(new int[]{2, 3, 0, 5, 3}, 32053));
         System.out.println(repsEqual(new int[]{9, 3, 1, 1, 2}, 32053));
         System.out.println(repsEqual(new int[]{0, 3, 2, 0, 5, 3}, 32053));
+        System.out.println(repsEqual(new int[]{3, 3, 2, 0, 5, 3}, 32053));
+        System.out.println(repsEqual(new int[]{3, 2, 0, 5, 3}, 332053));
     }
 
     public static int repsEqual(int[] a, int n) {
-        return 0;
+        for (int i=a.length-1; i>=0; i--) {
+            int rem = n % 10;
+            n = n / 10;
+            if (rem != a[i]) return 0;
+        }
+        return (n==0) ? 1 : 0;
     }
 }
